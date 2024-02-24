ed06a2b4fc369bd1f30302300a0cf7b6df6fdf70
Anlly12 <160801548+Anlly12@users.noreply.github.com>
2024-02-24 13:56:47 -0500

    Add files via upload

---
+++ JS.js
@@ -0,0 +1,8 @@
+const btnSingnIn = documen.getELementById("sign-in");
+      btnSingnIn= document.getElementById("sign-ip");
+      formRegistro = document.querySelector("Registro");
+      formLogin = documen.querySelector("Login");
+
+      btnSingnIn.addEventlistener("click ", e=> {
+        formRegistro.classList.add(hide)
+      })
